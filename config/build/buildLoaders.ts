ccf66717ef9e9baae8a5bf570ad3ca1771f2627c
Daniil Denisov <Cap.den2001@gmail.com>
2023-09-02 14:47:13 +0300

    added svgLoader and Button.tsx

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,10 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from './types/config'
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ['@svgr/webpack']
+  }
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -28,5 +32,14 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/
   }
 
-  return [typescriptLoader, cssLoaders]
+  const fileLoader = {
+    test: /\.(png|jpe?g|gif)$/i,
+    use: [
+      {
+        loader: 'file-loader'
+      }
+    ]
+  }
+
+  return [fileLoader, svgLoader, typescriptLoader, cssLoaders]
 }
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string
@@ -10,6 +11,7 @@ export const Navbar = ({ className }: NavbarProps) => {
   const linkTheme = AppLinkTheme.SECONDARY
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={linkTheme} to={'/'} className={cls.mainLink}>
           Главная
